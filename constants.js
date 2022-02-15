1617b5405a45adb8d309cbac6141ec1d98318cc8
adilzubarilw <adilzubarilw@gmail.com>
2022-02-15 18:17:15 +0500

    adil

--- constants.js
+++ constants.js
@@ -3,3 +3,13 @@ import { Dimensions, StyleSheet } from "react-native";
 export const wWidth = Dimensions.get("window").width;
 export const wHeight = Dimensions.get("window").height;
 export const pageBgColor = "rgb(50,50,50)";
+export const FIREBASE_API_KEY =
+  "AAAAcoQN7sw:APA91bG2Zuv2M7Pw8tJbEx6dJqzM5ravRdOfGxJDQQeov0GW-wX9csBGDPyCRsqwGq8ciHnW2hSgWX7LbYLI1AhwLzfE6h28cP-Fed9PdqAmODwJfHZ7v9rkshouWRe2JRv59imrTgde";
+export const firebaseConfig = {
+  apiKey: "AIzaSyCzKwXQSIVX2GC8nMqe7Xg-zqBgG0sx1ak",
+  projectId: "forex-signals-fa3d3",
+  storageBucket: "property-management-port-12157.appspot.com",
+  messagingSenderId: "483936075466",
+  appId: "1:483936075466:web:a99794bf8609e33d107e33",
+  measurementId: "G-J2T08JFN3S",
+};
